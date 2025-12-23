24f1a6645bfb9e106d802c4a1a8226a7d5df9a02
jchaconv <jchacon.vilela@gmail.com>
2025-12-23 12:23:32 -0500

    section6-7-liquibase sections 6 and 7 completed just theory and schema.sql adapted to pass ok the junit tests

--- sdjpa-intro/src/main/resources/schema.sql
+++ sdjpa-intro/src/main/resources/schema.sql
@@ -18,7 +18,7 @@ create table book_seq (
 
 insert into book_seq values ( 1 );
 
-/* to avoid error creation because don't have liquibase configurations */
+/* to avoid error creation because don't have liquibase and flyway configurations */
 create table author (
                       id bigint not null,
                       first_name varchar(255),
