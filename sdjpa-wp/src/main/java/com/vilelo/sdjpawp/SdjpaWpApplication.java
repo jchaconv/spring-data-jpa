0b8accde64558f201057fd6cee76e77b80e94f90
jchaconv <jchacon.vilela@gmail.com>
2025-12-29 15:49:54 -0500

    section2-legacy-database-mapping project config

---
+++ sdjpa-wp/src/main/java/com/vilelo/sdjpawp/SdjpaWpApplication.java
@@ -0,0 +1,13 @@
+package com.vilelo.sdjpawp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaWpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaWpApplication.class, args);
+	}
+
+}
